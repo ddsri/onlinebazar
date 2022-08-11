bc3946e9973357a8638071d57a9ee72d2a6b75bc
deshdeepaksrivastava <sridds008@gmail.com>
2022-08-11 11:59:51 +0530

    setting changes

--- OnlineBazar/settings.py
+++ OnlineBazar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d)9i-5_mjbh-qm0!uwo%d-e!v-mjwgprw50v$ag$a!l1h)%o23
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
